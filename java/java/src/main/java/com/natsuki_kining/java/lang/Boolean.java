f4bc22ecf80cc8bc80344adad4a5486309ff4719
zhangjianning <natsuki_kining@126.com>
2020-10-25 23:07:57 +0800

    还有这几个

---
+++ java/java/src/main/java/com/natsuki_kining/java/lang/Boolean.java
@@ -0,0 +1,10 @@
+package com.natsuki_kining.java.lang;
+
+/**
+ * TODO
+ *
+ * @Author natsuki_kining
+ * @Date 2020/10/25 23:04
+ **/
+public class Boolean {
+}
---
+++ java/java/src/main/java/com/natsuki_kining/java/lang/Float.java
@@ -0,0 +1,10 @@
+package com.natsuki_kining.java.lang;
+
+/**
+ * TODO
+ *
+ * @Author natsuki_kining
+ * @Date 2020/10/25 23:06
+ **/
+public class Float {
+}
