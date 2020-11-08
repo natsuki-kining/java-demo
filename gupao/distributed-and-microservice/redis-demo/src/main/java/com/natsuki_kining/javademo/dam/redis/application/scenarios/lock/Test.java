9bebcc93927a8c3b94f86309e4602fe5149b52ac
zhangjianning <natsuki_kining@126.com>
2020-11-08 13:07:12 +0800

    分布式锁测试

--- gupao/distributed-and-microservice/redis-demo/src/main/java/com/natsuki_kining/javademo/dam/redis/application/scenarios/lock/Test.java
+++ gupao/distributed-and-microservice/redis-demo/src/main/java/com/natsuki_kining/javademo/dam/redis/application/scenarios/lock/Test.java
@@ -2,6 +2,8 @@ package com.natsuki_kining.javademo.dam.redis.application.scenarios.lock;
 
 import com.natsuki_kining.javademo.dam.redis.utils.JedisUtil;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * TODO
  *
@@ -11,67 +13,60 @@ import com.natsuki_kining.javademo.dam.redis.utils.JedisUtil;
 public class Test {
 
     private static String dataKey = "dataKey";
-    private static int count = 1000;
+    private static int count = 100;
+    private static int threadCount = 3;
+    private static CountDownLatch unLockCountDownLatch = new CountDownLatch(threadCount);
+    private static CountDownLatch lockCountDownLatch = new CountDownLatch(threadCount);
 
     public static void main(String[] args) throws InterruptedException {
+
         //并发对资源进行修改
         //不加锁
         notLock();
-
-        Thread.sleep(2000);
-
+        unLockCountDownLatch.await();
         String data = JedisUtil.getJedisUtil().get(dataKey);
         System.out.println("data:"+data);
 
+        lock();
+        lockCountDownLatch.await();
+        data = JedisUtil.getJedisUtil().get(dataKey);
+        System.out.println("data:"+data);
+    }
+
+    public static void unLockThread(){
+        new Thread(()->{
+            for (int i = count; i > 0; i--) {
+                int data = Integer.parseInt(JedisUtil.getJedisUtil().get(dataKey));
+                JedisUtil.getJedisUtil().set(dataKey,(++data)+"");
+            }
+            unLockCountDownLatch.countDown();
+        }).start();
+    }
+
+    public static void lockThread(){
+        new Thread(()->{
+            RedisLock lock = new RedisDistributedLock();
+            lock.lock();
+            for (int i = count; i > 0; i--) {
+                int data = Integer.parseInt(JedisUtil.getJedisUtil().get(dataKey));
+                JedisUtil.getJedisUtil().set(dataKey,(++data)+"");
+            }
+            lock.unlock();
+            lockCountDownLatch.countDown();
+        }).start();
     }
 
     public static void notLock(){
-        new Thread(()->{
-            for (int i = count; i > 0; i--) {
-                JedisUtil.getJedisUtil().incrBy(dataKey,1);
-            }
-        }).start();
-
-        new Thread(()->{
-            for (int i = count; i > 0; i--) {
-                JedisUtil.getJedisUtil().incrBy(dataKey,1);
-            }
-        }).start();
-
-        new Thread(()->{
-            for (int i = count; i > 0; i--) {
-                JedisUtil.getJedisUtil().incrBy(dataKey,1);
-            }
-        }).start();
+        JedisUtil.getJedisUtil().set(dataKey,"0");
+        for (int i = 0; i < threadCount; i++) {
+            unLockThread();
+        }
     }
 
     public static void lock(){
-        new Thread(()->{
-            RedisLock lock = new RedisDistributedLock();
-            lock.lock();
-            for (int i = count; i > 0; i--) {
-                JedisUtil.getJedisUtil().incrBy(dataKey,1);
-            }
-            lock.unlock();
-        }).start();
-
-        new Thread(()->{
-            RedisLock lock = new RedisDistributedLock();
-            lock.lock();
-            for (int i = count; i > 0; i--) {
-                JedisUtil.getJedisUtil().incrBy(dataKey,1);
-            }
-            lock.unlock();
-
-        }).start();
-
-        new Thread(()->{
-            RedisLock lock = new RedisDistributedLock();
-            lock.lock();
-            for (int i = count; i > 0; i--) {
-                JedisUtil.getJedisUtil().incrBy(dataKey,1);
-            }
-            lock.unlock();
-        }).start();
+        JedisUtil.getJedisUtil().set(dataKey,"0");
+        for (int i = 0; i < threadCount; i++) {
+            lockThread();
+        }
     }
 }
