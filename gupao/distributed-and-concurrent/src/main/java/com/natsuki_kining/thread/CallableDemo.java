1dcb4f869314efa89a2d7adc7cb7229578e2348f
zhangjianning <natsuki_kining@126.com>
2020-11-12 20:27:12 +0800

    线程

---
+++ gupao/distributed-and-concurrent/src/main/java/com/natsuki_kining/thread/CallableDemo.java
@@ -0,0 +1,27 @@
+package com.natsuki_kining.thread;
+
+import java.util.concurrent.*;
+
+/**
+ * 带返回值的线程
+ *
+ * @Author natsuki_kining
+ * @Date 2020/11/12 20:17
+ **/
+public class CallableDemo implements Callable<String> {
+
+    public String call() throws Exception {
+        Thread.sleep(200);
+        System.out.println("call");
+        return "SUCCESS";
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        CallableDemo callableDemo = new CallableDemo();
+        Future<String> future = executorService.submit(callableDemo);
+        //阻塞
+        System.out.println(future.get());
+        System.out.println("main");
+    }
+}
---
+++ gupao/distributed-and-concurrent/src/main/java/com/natsuki_kining/thread/ThreadDemo.java
@@ -0,0 +1,36 @@
+package com.natsuki_kining.thread;
+
+/**
+ * 线程demo
+ *
+ * @Author natsuki_kining
+ * @Date 2020/11/12 20:07
+ **/
+public class ThreadDemo extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("run");
+    }
+
+    public static void main(String[] args) {
+        Thread runnableDemo = new Thread(new RunnableDemo());
+        ThreadDemo demo = new ThreadDemo();
+        demo.start();
+        runnableDemo.start();
+        System.out.println("main");
+    }
+
+
+    static class RunnableDemo implements Runnable{
+
+        public void run() {
+            System.out.println("Runnable");
+        }
+    }
+}
